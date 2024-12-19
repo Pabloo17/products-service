f9b6bda474b2faed49b0480e2faea1c82f0cbb6a
Pablo <pablochester92@gmail.com>
2024-12-19 17:40:52 +0100

    created products entity

---
+++ src/main/java/com/servicio/products/entities/Product.java
@@ -0,0 +1,31 @@
+package com.servicio.products.entities;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private Double price;
+
+    @Column(name = "create_at")
+    private LocalDate createAt;
+
+}
