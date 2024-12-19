950e2d7f686df556ed9887b9d8bc70291048ac3a
Pablo <pablochester92@gmail.com>
2024-12-19 17:11:12 +0100

    Initial commit

---
+++ src/test/java/com/servicio/products/ProductsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.servicio.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
